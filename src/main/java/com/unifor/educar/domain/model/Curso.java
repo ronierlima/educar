4fa058feae3fa17d471cfa259c275182171adf1b
Ronier Lima <ronier.lim@gmail.com>
2023-10-24 13:55:16 -0300

    add entidades

---
+++ src/main/java/com/unifor/educar/domain/model/Curso.java
@@ -0,0 +1,13 @@
+package com.unifor.educar.domain.model;
+
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Curso {
+}
