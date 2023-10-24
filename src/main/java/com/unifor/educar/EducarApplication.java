2bb574b72a353c785551078e3b2a61b8f2556210
Ronier Lima <ronier.lim@gmail.com>
2023-10-24 10:24:09 -0300

    Inicializando projeto

---
+++ src/main/java/com/unifor/educar/EducarApplication.java
@@ -0,0 +1,13 @@
+package com.unifor.educar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unifor/educar/EducarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifor.educar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
