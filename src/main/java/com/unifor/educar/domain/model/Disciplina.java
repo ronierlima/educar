5fe52f1816bd7760b607220bf3f359462501a5c4
Ronier Lima <ronier.lim@gmail.com>
2023-10-24 13:35:31 -0300

    criar repository e ajusta model de disciplina

--- src/main/java/com/unifor/educar/domain/model/Disciplina.java
+++ src/main/java/com/unifor/educar/domain/model/Disciplina.java
@@ -1,8 +1,21 @@
 package com.unifor.educar.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Disciplina {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String codigo;
+    private String nome;
+    private String descricao;
     private Long cargaHoraria;
 }
